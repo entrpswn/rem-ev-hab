aa740b99ac6f78bb79a908c6d1f475ad65b55985
Vova <5251409+entrpswn@users.noreply.github.com>
2024-10-08 11:36:47 +0200

    Initial Commit

---
+++ rem-1Tests/rem_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  rem_1Tests.swift
+//  rem-1Tests
+//
+//  Created by Vova Kondriianenko on 08.10.2024.
+//
+
+import Testing
+@testable import rem_1
+
+struct rem_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
